cf8386cf6224c9d48127f9f23339b13bd543a729
David Van Der Beek <earlynovrock@gmail.com>
2011-10-03 10:27:14 -0500

    validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, :presence => true
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :title, :uniqueness => true, :length => {
+    :minimum => 10,
+    :message => 'needs 10 chars sucka'
+  }
+  validates :image_url, :format => {
+    :with => %r{\.(gif|jpg|png)$}i,
+    :message => 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
